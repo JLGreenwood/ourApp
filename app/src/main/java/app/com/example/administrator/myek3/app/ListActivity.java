ca6adaa097fd07b8277a09ed51d8995b0b808b76
sfranke <sfr4nke@gmail.com>
2017-03-09 00:34:11 +0100

    asdff

--- app/src/main/java/app/com/example/administrator/myek3/app/ListActivity.java
+++ app/src/main/java/app/com/example/administrator/myek3/app/ListActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -24,13 +25,13 @@ import static app.com.example.administrator.myek3.app.R.id.fab;
 public class ListActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-
     FloatingActionButton fab;
     ListView listView;
 
     List<ShoppingList> shoppingListList;
     ShoppingListAdapter shoppingListAdapter;
-
+    ShoppingList sl;
+    ArticleAdapter articleAdapter;
 
     Context ctx = null;
 
@@ -43,6 +44,24 @@ public class ListActivity extends AppCompatActivity
 
         fab = (FloatingActionButton) findViewById(R.id.fab_list);
         listView = (ListView) findViewById(R.id.listlist);
+
+        articleAdapter = new ArticleAdapter(sl.getShoppingListArticles(), this);
+        listView.setAdapter(articleAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Article art = sl.getShoppingListArticles().get(position);
+                if (art.isArticleChecked()) {
+//                    art.setArticleChecked(false);
+                    articleAdapter.notifyDataSetChanged();
+                }
+                else {
+//                    art.setArticleChecked(true);
+                    articleAdapter.notifyDataSetChanged();
+                }
+            }
+        });
 
         // Disabled because of unclear NullPointerException. Fix needed!!
         // registerForContextMenu(listView);
@@ -99,7 +118,6 @@ public class ListActivity extends AppCompatActivity
         if (id == R.id.action_sort) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -122,10 +140,8 @@ public class ListActivity extends AppCompatActivity
             mysession.setFirstTimeLaunch(true);
             Intent intent = new Intent(this, WelcomeActivity.class);
             startActivity(intent);
-        } else if (id == R.id.nav_imp_list) {
-
+//        } else if (id == R.id.nav_imp_list) {
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.list_drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
